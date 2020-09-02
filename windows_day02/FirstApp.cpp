faf31653f0273579df664752e27da96ce3c87fca
yongma16 <17685043634@163.com>
2020-09-02 21:49:28 +0800

    配置环境
    测试第一个工程

---
+++ windows_day02/FirstApp.cpp
@@ -0,0 +1,15 @@
+#include<Windows.h>
+#include<tchar.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, PSTR pszCmdLine, int nCmdShow)
+{
+	//DWORD GetLastError();
+	//VOID SetLastError(DWORD dwErrCode);
+	//DWORD FormatMessage;
+	//DWORD dwerror = GetDlgItemInt(hwnd, IDC_ERRORCODE, NULL, FALSE);
+	//查看内核对象
+	//CreateFile();//创建一个内核对象
+	//HANDLE processdemo1 = CreateFile();
+	//MessageBox(NULL, _T("测试\n网站：yongma16.xyz"), _T("yongma16.xyz"), MB_ABORTRETRYIGNORE | MB_ICONWARNING | MB_DEFBUTTON2);//返回一个进程相关的句柄
+	
+	return 0;
+}
