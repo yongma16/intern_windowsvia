cf79f825218b3cbe277353186107ae7c2984816f
yongma16 <17685043634@163.com>
2020-09-05 17:21:20 +0800

    添加工文件

---
+++ Windows_day05/Windows_day05.cpp
@@ -0,0 +1,21 @@
+﻿//windows编程的案例
+#include<Windows.h>
+#include<stdio.h>
+class CSomeObj
+{
+public:
+	CSomeObj()
+	{
+		printf("Constrctor\r\n");
+	}
+	~CSomeObj()
+	{
+		printf("Destructor\r\n");
+	}
+};
+CSomeObj g_GlobalObj;
+void main(int argc, char* argv)
+{
+	CSomeObj LocalObj;
+	ExitProcess(0);//
+}
